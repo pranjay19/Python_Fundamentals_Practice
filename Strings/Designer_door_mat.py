bd178d30dbed2642383e546a871edded193735e0
Pranjay19 <pranjayguleria@gmail.com>
2026-08-07 12:34:00 +1000

    solved question: designer_door_mat

---
+++ Strings/Designer_door_mat.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# Read input from STDIN
+row, column = map(int, input().split())
+
+#first_half
+
+for i in range(1,row,2):
+    pattern=".|."*i
+    print(pattern.center(column,"-"))
+
+#centre
+print("WELCOME".center(column,"-"))
+
+
+#second_half
+for i in range(row-2, -1, -2):
+    pattern = '.|.' * i
+    print(pattern.center(column, '-'))
