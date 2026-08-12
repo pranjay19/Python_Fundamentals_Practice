606f881c91dede0a36cfc666da1c393600a1e8f6
Pranjay19 <pranjayguleria@gmail.com>
2026-08-12 23:16:36 +1000

    solved question Alphabet_rangoli

---
+++ Strings/Alphabet_rangoli.py
@@ -0,0 +1,21 @@
+def print_rangoli(size):
+    rows = size*2-1
+    columns = (n*2-1)+(n*2-2)
+    k = (rows // 2)+1
+    j = k - 1
+
+    for i in range(1,rows+1):
+        if(k >= i):
+            l1 = [chr(x) for x in range((96+size),(96+size-i),-1)]
+            l2 = [chr(x) for x in range((96+size-i+2),(96+size+1))]       
+        else:
+            l1 = [chr(x) for x in range((96+size),(96+size-j),-1)]
+            l2 = [chr(x) for x in range((96+size-j+2),(96+size+1))]          
+            j -= 1
+        l = l1+l2
+        string = '-'.join(l)
+        print(string.center(columns,'-'))
+    
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
